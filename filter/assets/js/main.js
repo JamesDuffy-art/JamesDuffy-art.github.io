0814fe66943540f10f2af2785a0a082ee8643c73
JamesDuffy-art <70005610+JamesDuffy-art@users.noreply.github.com>
2020-09-17 17:03:01 +1000

    commit

--- filter/assets/js/main.js
+++ filter/assets/js/main.js
@@ -1,37 +1,30 @@
 $(document).ready(function(){
 	console.log('hello world');
 
-	$('.btn-box').click(function(){
-		$('.box').toggleClass('active');
-	});
-
-	$('.box').click(function(){
-		$(this).toggleClass('active');
-	});
 
 	$('.btn-hor').click(function(){
 		$('.item').addClass('hide');
-		$('.item.mag').removeClass('hide');
+		$('.item.hor').removeClass('hide');
 	});
 
 	$('.btn-sup').click(function(){
 		$('.item').addClass('hide');
-		$('.item.red').removeClass('hide');
+		$('.item.sup').removeClass('hide');
 	});
 
 	$('.btn-ama').click(function(){
 		$('.item').addClass('hide');
-		$('.item.plan').removeClass('hide');
+		$('.item.ama').removeClass('hide');
 	});
 
 	$('.btn-prop').click(function(){
 		$('.item').addClass('hide');
-		$('.item.shape').removeClass('hide');
+		$('.item.prop').removeClass('hide');
 	});
 
 	$('.btn-pat').click(function(){
 		$('.item').addClass('hide');
-		$('.item.scan').removeClass('hide');
+		$('.item.pat').removeClass('hide');
 	});
 
 	$('.btn-reset').click(function(){
