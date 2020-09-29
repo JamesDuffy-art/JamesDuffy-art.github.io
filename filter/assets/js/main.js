d177e1e58838fbbadfd67f9a553b5d3407f8d775
JamesDuffy-art <70005610+JamesDuffy-art@users.noreply.github.com>
2020-09-29 16:50:57 +1000

    up

--- filter/assets/js/main.js
+++ filter/assets/js/main.js
@@ -1,49 +1,44 @@
-$(document).ready(function(){
-	console.log('hello world');
+$(document).ready(function () {
+  console.log("hello world");
 
-	$('.btn-box').click(function(){
-		$('.box').toggleClass('active');
-	});
+  $(".btn-box").click(function () {
+    $(".box").addClass("active");
+  });
 
-	$('.box').click(function(){
-		$(this).toggleClass('active');
-	});
+  $(".btn-slide").click(function () {
+    $(".slide").toggleClass("active");
+  });
 
-	$('.btn-slide').click(function(){
-		$('.slide').toggleClass('active');
-	});
+  $(".box").click(function () {
+    $(this).toggleClass("active");
+  });
 
-	$('.btn-alert').click(function(){
-		$('.alert').toggleClass('active');
-	});
-	
-	$('.btn-hor').click(function(){
-		$('.item').addClass('hide');
-		$('.item.hor').removeClass('hide');
-	});
+  $(".btn-hor").click(function () {
+    $(".item").addClass("hide");
+    $(".item.hor").removeClass("hide");
+  });
 
-	$('.btn-sup').click(function(){
-		$('.item').addClass('hide');
-		$('.item.sup').removeClass('hide');
-	});
+  $(".btn-sup").click(function () {
+    $(".item").addClass("hide");
+    $(".item.sup").removeClass("hide");
+  });
 
-	$('.btn-ama').click(function(){
-		$('.item').addClass('hide');
-		$('.item.ama').removeClass('hide');
-	});
+  $(".btn-ama").click(function () {
+    $(".item").addClass("hide");
+    $(".item.ama").removeClass("hide");
+  });
 
-	$('.btn-prop').click(function(){
-		$('.item').addClass('hide');
-		$('.item.prop').removeClass('hide');
-	});
+  $(".btn-prop").click(function () {
+    $(".item").addClass("hide");
+    $(".item.prop").removeClass("hide");
+  });
 
-	$('.btn-pat').click(function(){
-		$('.item').addClass('hide');
-		$('.item.pat').removeClass('hide');
-	});
+  $(".btn-pat").click(function () {
+    $(".item").addClass("hide");
+    $(".item.pat").removeClass("hide");
+  });
 
-	$('.btn-reset').click(function(){
-		$('.item').removeClass('hide');
-	});
-
+  $(".btn-reset").click(function () {
+    $(".item").removeClass("hide");
+  });
 });
