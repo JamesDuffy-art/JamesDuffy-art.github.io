40fac3d53a376d07e7016a057bde77ba791815ad
JamesDuffy-art <70005610+JamesDuffy-art@users.noreply.github.com>
2021-06-08 21:36:28 +1000

    update

---
+++ website/assets/js/main.js
@@ -0,0 +1,15 @@
+var james = [
+    <img src="assets/images/1x/Asset 11.png"></img>,
+    <img src="assets/images/1x/Asset 12.png"></img>,
+    <img src="assets/images/1x/Asset 13.png"></img>,
+    <img src="assets/images/1x/Asset 14.png"></img>,
+    <img src="assets/images/1x/Asset 15.png"></img>,
+    <img src="assets/images/1x/Asset 16.png"></img>,
+    <img src="assets/images/1x/Asset 17.png"></img>,
+    <img src="assets/images/1x/Asset 18.png"></img>,
+    <img src="assets/images/1x/Asset 19.png"></img>,
+    <img src="assets/images/1x/Asset 20.png"></img>,
+    <img src="assets/images/1x/Asset 21.png"></img>,
+    <img src="assets/images/1x/Asset 22.png"></img>,
+
+  ];
